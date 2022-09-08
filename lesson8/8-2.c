b2aef3887c550123f764ac2abcd195d00c85fa7e
Andrey Fedorov <87073832+AndreyFedorov78@users.noreply.github.com>
2022-09-08 22:43:58 +0300

    Исправлена ошибка в задании 8-2
    Внешний цикл (i) надо было начинать с 1 а не с 0!

--- lesson8/8-2.c
+++ lesson8/8-2.c
@@ -7,15 +7,16 @@
 
 uint32_t bitCount(uint32_t n, uint32_t k){
     uint32_t count=0;
-    for (uint32_t i = 0; i <= n; i++) {
+    for (uint32_t i = 1; i <= n; i++) {
         uint8_t flag = 0;
         uint32_t count_0 = 0;
         for (int8_t j = 31; j>=0; j--) {
             flag = flag || ((1<<j) & i) ;  // если флаг не установлен ищем старшую единицу
             count_0 += (((1<<j) & i) == 0) && flag ;
+
         }
         if (count_0 == k) count++;
-                // if (count_0 == k) printf("%u - ", count);;
+               
     }
     //if ((0 == n) && (k==0)) count=1;
     if ((0 == n) && (k==1)) count=1;
